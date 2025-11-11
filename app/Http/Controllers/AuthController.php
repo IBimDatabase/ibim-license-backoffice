3aa7f05ba98ed7d5f18abb9e9ed39f79d8744d6c
Abdul Rehman <abdulrehmaan.ajmer@gmail.com>
2025-11-11 21:53:30 +0530

    remove debug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
 
         public function authenticate(Request $request)
         {
-            var_dump(bcrypt('admin123'));exit;
+            //var_dump(bcrypt('admin123'));exit;
             try {
                 $credentials = $request->only('user_name', 'password'); // use 'email', not 'user_name'
 
